a8863ef828d2bdbeafd42658d46f1f0e85662c5e
victor dias <victor.dias@meumobi.com>
2018-11-14 04:25:14 +0000

    feat: add sidemenu and login page with menu disabled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, MenuController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router, RouterEvent, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private menuCtrl: MenuController,
+    private router: Router
   ) {
     this.initializeApp();
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe((event: RouterEvent) => {
+      if (event instanceof NavigationEnd && event.url === '/login') {
+        this.menuCtrl.enable(false);
+      }
+    });
   }
 
   initializeApp() {
